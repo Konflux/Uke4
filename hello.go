d955a913e1e8f21eb1c7b34a8d9fbecb8e3ad152
tarhavn <ggtarh16@student.uia.no>
2017-01-30 12:58:23 +0100

    Update hello.go

--- hello.go
+++ hello.go
@@ -6,5 +6,6 @@ func main(){
   fmt.Println("Vi er gruppe 9, Dyreparken \nog vi består av:")
   fmt.Println("Marius")
   fmt.Println("Nikolai")
+    fmt.Println("Gisle Tarhavn")
   //legg til navnene deres nedover her i et Println statement
 }
