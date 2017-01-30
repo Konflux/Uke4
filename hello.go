adbfae99276734f65bab0b1191a1982f28de9ca1
Nikolai Holmen Dyb <Nikolai Holmen Dyb>
2017-01-30 12:41:12 +0100

    la inn navn

--- hello.go
+++ hello.go
@@ -5,5 +5,6 @@ import "fmt"
 func main(){
   fmt.Println("Vi er gruppe 9, Dyreparken \nog vi består av:")
   fmt.Println("Marius")
+  fmt.Println("Nikolai")
   //legg til navnene deres nedover her i et Println statement
 }
